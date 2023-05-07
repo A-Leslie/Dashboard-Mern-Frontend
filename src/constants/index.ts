d4d91653a8ee895ceec0d966eb51fb798e0550c4
_l.esli.e_ <anneuhiriwe@gmail.com>
2023-05-07 19:46:31 +0200

    end

---
+++ src/constants/index.ts
@@ -0,0 +1,23 @@
+export const propertyReferralsInfo=[
+    {
+        title:'Social Media',
+        percentage:70,
+        color:'#6C5DD3'
+    },
+    {
+        title:'Marketplace',
+        percentage:56,
+        color:'#7FBA7A'
+    },
+    {
+        title:'Websites',
+        percentage:65,
+        color:'#FFCE73'
+    },
+    {
+        title:'Digital Ads',
+        percentage:80,
+        color:'#FFA2C0'
+    }
+
+]
---
+++ src/pages/my-profile.tsx
@@ -0,0 +1,66 @@
+// import { useGetIdentity, useOne } from "@refinedev/core";
+
+// import { Profile } from "components";
+
+// const MyProfile = () => {
+//     const { data: user } = useGetIdentity({
+//         v3LegacyAuthProviderCompatible: true,
+//     });
+//     const { data, isLoading, isError } = useOne({
+//         resource: "users",
+//         id: user?.userid,
+//     });
+
+//     const myProfile = data?.data ?? [];
+
+//     if (isLoading) return <div>loading...</div>;
+//     if (isError) return <div>error...</div>;
+
+//     return (
+//         <Profile
+//             type="My"
+//             name={myProfile.name}
+//             email={myProfile.email}
+//             avatar={myProfile.avatar}
+//             properties={myProfile.allProperties}
+//         />
+//     );
+// };
+
+// export default MyProfile;
+
+
+
+
+
+import { useGetIdentity, useOne } from "@refinedev/core";
+
+import { Profile } from "components";
+
+const MyProfile = () => {
+    const { data: user } = useGetIdentity({
+        v3LegacyAuthProviderCompatible: true,
+    });
+    const { data, isLoading, isError } = useOne({
+        resource: "users",
+        id: user?.userid || "",
+    });
+
+    const myProfile = data?.data ?? {};
+
+    if (!user) return <div>loading...</div>;
+    if (isLoading) return <div>loading...</div>;
+    if (isError) return <div>error...</div>;
+
+    return (
+        <Profile
+            type="My"
+            name={myProfile.name}
+            email={myProfile.email}
+            avatar={myProfile.avatar}
+            properties={myProfile.allProperties}
+        />
+    );
+};
+
+export default MyProfile;
